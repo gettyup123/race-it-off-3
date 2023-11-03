096eca4e37e2e728c976148de1bc87d14ae65a31
gettyup123 <148966928+gettyup123@users.noreply.github.com>
2023-11-03 14:04:43 +0100

    Update index.java

--- index.java
+++ index.java
@@ -4,9 +4,3 @@ document.addEventListener('touchmove', (event) => {
   carPosition = Math.max(0, Math.min(container.clientWidth - car.clientWidth, carPosition));
   car.style.left = carPosition + 'px';
 });
-  } else if (event.key === 'A') {
-        isInvincible = true;
-      } else if (event.key === 'F') {
-        isInvincible = false;
-      }
-    });
